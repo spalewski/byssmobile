d2c95aa818c323cd0131571abfb324387d762ad5
SebastianPal <spalewski@gmail.com>
2019-08-23 12:33:16 +0200

    Initial Commit

---
+++ byssmobile/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  byssmobile
+//
+//  Created by Sebastian Palewski on 23/08/2019.
+//  Copyright © 2019 Sebastian Palewski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
